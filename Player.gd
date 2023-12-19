a0f1d59ea839c4e85e145dd7b93f7fd909e124a7
joyfaithh <122395454+joyfaithh@users.noreply.github.com>
2023-12-19 12:29:34 -0800

    player scene completed

--- Player.gd
+++ Player.gd
@@ -1,20 +1,21 @@
 extends Area2D
 
-@export var speed = 100
+@export var speed = 400
 
 func _process(delta):
-	pass
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y +=1
+		velocity.y -=1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -=1
 	if Input.is_action_pressed("move_down"):
-		velocity.y -=1
+		velocity.y +=1
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed 
 	position += velocity * delta
-		
+	
+	
+	
